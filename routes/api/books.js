8acefc2c59bf78f3950c97e4c91b7b21f23abbda
roldie9 <aroldoreyes9@gmail.com>
2020-05-13 20:20:48 -0500

    routes fixed and removed description from json package

--- routes/api/books.js
+++ routes/api/books.js
@@ -1,6 +1,15 @@
 const router = require("express").Router();
-const booController = require("../../controllers/bookController");
+const bookController = require("../../controllers/bookController");
 
 // match with "/api/books"
+router.route("/")
+    .get(bookController.findAll)
+    .post(bookController.create);
 
 // match with "api/books/:id"
+router.route("/:id")
+    .get(bookController.findById)
+    .put(bookController.update)
+    .delete(bookController.remove);
+
+module.exports = router;
---
+++ routes/api/index.js
@@ -0,0 +1,17 @@
+const path = require("path");
+const router = require("express").Router();
+const bookRoutes = require("./books");
+const googleRoutes = require(".google");
+
+// book route
+router.use("/books", bookRoutes);
+
+// google route
+router.use("/google", googleRoutes);
+
+// render html page if not book or google 
+router.use(function(req, res) {
+    res.sendFile(path.join(__dirname, "../../client/build/index.html"));
+});
+
+module.exports = router;
