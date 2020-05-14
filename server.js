38c187d52b321a87a6b157f772a390c4ce341400
roldie9 <aroldoreyes9@gmail.com>
2020-05-13 22:37:24 -0500

    working on api call

--- server.js
+++ server.js
@@ -21,7 +21,8 @@ mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/googlebooks",
   {
     useCreateIndex: true,
-    useNewUrlParser: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true
   }
 );
 
